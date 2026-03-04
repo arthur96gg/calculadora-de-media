92ea7cb187ec7bba2b8386ce6e6f696d6f6ce596
arthur96gg <arthur.9166@colegioprogresso.g12.br>
2026-03-04 11:22:20 -0300

    pegamos os dados

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("deu certo")
+const nota1 = document.getElementById("nota1")
+const nota2 = document.getElementById("nota2")
+const button = document.getElementById("button")
+
+function CalculadoraMedia(){ 
+
+    const Valornota1 = Number (nota1.value)
+    const Valornota2 = Number (nota2.value)
+
+    const media =(Valornota1 + Valornota2)/2
+     console.log(media)
+}
+button.addEventListener("click",CalculadoraMedia)
